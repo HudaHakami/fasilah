395ce7323834207317efe4e2eff3aa3864ef170b
Sara0algethami <98253340+Sara0algethami@users.noreply.github.com>
2023-09-21 13:25:39 +0300

    Registraion
    Reset password page

---
+++ lib/features/registration/reset_password/view.dart
@@ -0,0 +1,71 @@
+//import 'package:fasilah/shared/components/navigator.dart';
+import 'package:flutter/material.dart';
+import '../../../shared/components/components.dart';
+import '../../../shared/components/constants.dart';
+import '../../../shared/styles/colors.dart';
+
+class ResetPassword extends StatefulWidget {
+  const ResetPassword({Key? key}) : super(key: key);
+
+  @override
+  State<ResetPassword> createState() => _ResetPasswordState();
+}
+
+class _ResetPasswordState extends State<ResetPassword> {
+  final formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Forget password'),
+        // leading: IconButton(
+        //     //onPressed: () => navigatePop(context),
+        //     icon: const Icon(
+        //       Icons.arrow_back,
+        //     )),
+      ),
+      body: BackgroundBox(
+        widget: Column(
+          children: [
+            Form(
+              key: formKey,
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  TextFieldTemplate(
+                    hintText: 'example@gmail.com',
+                    icon: Icons.mail_outlined,
+                    keyType: TextInputType.emailAddress,
+                    controller: emailController,
+                    validator: (String? value) {
+                      if (value!.isEmpty) {
+                        return 'please enter your email';
+                      } else if (value.length < 5) {
+                        return 'please write email in correct way';
+                      } else if (!value.toString().contains('@')) {
+                        return 'email must contain @';
+                      }
+                    },
+                    labelText: 'Email',
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  ButtonTemplate(
+                      minwidth: width(context, 2),
+                      color: AppColors.brown,
+                      text1: 'send',
+                      onPressed: () => {}),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
